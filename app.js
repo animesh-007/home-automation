2cd5dd0d4a6449b351ea80840697042f8c4aa26b
animesh-007 <animeshagarwal7@gmail.com>
2020-12-26 06:44:06 +0530

    removed bearer

--- app.js
+++ app.js
@@ -56,8 +56,7 @@ app.intent('devicecontrol', (conv,{devicename,devicestatus}) => {
             url: ip_add+url,
             headers: 
             { 
-                'cache-control': 'no-cache',
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZDA3N2I0YmE2ODRhYzcwYTk5ZGNjYyIsIm5hbWUiOiJBbmltZXNoIiwiYXZhdGFyIjoiLy93d3cuZ3JhdmF0YXIuY29tL2F2YXRhci8yMmNhMzA4OGM2MzllNDQ1MDBmZmIzZjk3Yzk1YjQ0Mz9zPTIwMCZyPXBnIiwiaWF0IjoxNTk0MTU0NDcyLCJleHAiOjE1OTQxNTgwNzJ9.w-zroBn4UEf7Hx-15RxPvXCpTUqxvnnCdwxlX2EJmZY'
+                'cache-control': 'no-cache'
             } 
         };
     
